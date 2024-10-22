14600b415ca7e2c2e91a0e9a18b6157ffad5be49
Jaye <jaye1248276@gmail.com>
2024-10-22 18:41:36 +0800

    fix: 兼容cjs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'index',
       fileName: 'index',
-      formats: ['es']
+      formats: ['es', 'cjs']
     },
     rollupOptions: {
       external: ['node:fs', 'node:path', 'chalk']
